5b4012a1e230108ce41f96edae5a754c0b7b0a57
shinyeji28 <xntm10@naver.com>
2024-01-18 15:56:32 +0900

    chore: 프로젝트 세팅

---
+++ backend/Exhale/src/main/java/com/ssafy/exhale/ExhaleApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.exhale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExhaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExhaleApplication.class, args);
+	}
+
+}
