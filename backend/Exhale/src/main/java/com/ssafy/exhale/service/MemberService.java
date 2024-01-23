57a550384417300dd4d173afc1bc1b5b0b6dbd58
shinyeji28 <xntm10@naver.com>
2024-01-23 22:35:59 +0900

    feat: 플랫폼 회원가입 구현 (비밀번호 암호화)

---
+++ backend/Exhale/src/main/java/com/ssafy/exhale/service/MemberService.java
@@ -0,0 +1,39 @@
+package com.ssafy.exhale.service;
+
+import com.ssafy.exhale.domain.Member;
+import com.ssafy.exhale.dto.requestDto.JoinRequest;
+import com.ssafy.exhale.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    public void join(JoinRequest joinRequest) {
+
+        String loginId = joinRequest.getLogin_id();
+        String password = joinRequest.getPassword();
+
+        Boolean isExist = memberRepository.existsByLoginId(loginId);
+        if (isExist) {
+            // todo: 이미 존재하는 아이디 response 처리
+            return;
+        }
+
+        Member member = new Member();
+        member.setName(joinRequest.getName());
+        member.setBirth(joinRequest.getBirth());
+        member.setEmailDomain(joinRequest.getEmail_domain());
+        member.setEmailId(joinRequest.getEmail_id());
+        member.setLoginId(loginId);
+        member.setPassword(bCryptPasswordEncoder.encode(password));
+        member.setRole("ROLE_USER");
+
+        memberRepository.save(member);
+    }
+
+
+}
