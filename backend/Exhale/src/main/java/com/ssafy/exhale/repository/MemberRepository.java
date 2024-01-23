3b3900a6bf1ac04747c9e23c75e2c9905f2f49a7
shinyeji28 <xntm10@naver.com>
2024-01-23 22:38:09 +0900

    refactor: 아이디 존재 여부 repository에 추가

--- backend/Exhale/src/main/java/com/ssafy/exhale/repository/MemberRepository.java
+++ backend/Exhale/src/main/java/com/ssafy/exhale/repository/MemberRepository.java
@@ -4,5 +4,7 @@ import com.ssafy.exhale.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Boolean existsByLoginId(String login_id);
     Member findByLoginId(String login_id);
 }
