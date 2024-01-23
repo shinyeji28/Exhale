5e34cdb88f664dfca1e4a50f7723ca66a8b73923
HuiJung <tom1997tom@gmail.com>
2024-01-23 13:13:32 +0900

    feat: Board Entity 구현

---
+++ backend/Exhale/src/main/java/com/ssafy/exhale/domain/Board.java
@@ -0,0 +1,20 @@
+package com.ssafy.exhale.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "board")
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_id")
+    private int boardId;
+    @Column(name = "category_name")
+    private String categoryName;
+}
