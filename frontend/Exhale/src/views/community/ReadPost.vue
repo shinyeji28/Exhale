b66f78907a4a115b2238202032d2a2a5d44b0350
Hana Na <gksk3185@gmail.com>
2024-01-23 11:09:56 +0900

    test: 커뮤니티 설계 테스트[#194]

--- frontend/Exhale/src/views/community/ReadPost.vue
+++ frontend/Exhale/src/views/community/ReadPost.vue
@@ -1,6 +1,13 @@
 <template>
     <div>
-
+        <h1>날;숨(커뮤니티)</h1>
+    </div>
+    <div id="breadcrum">
+        <RouterLink :to="{name: 'MainPage'}">메인 홈</RouterLink>
+        >
+        <RouterLink :to="{name: 'ReadPost'}">커뮤니티</RouterLink>
+        >
+        <!-- <RouterLink :to="{name: ''}">환자이야기</RouterLink> -->
     </div>
 </template>
 
@@ -9,5 +16,7 @@
 </script>
 
 <style lang="scss" scoped>
-
+    #breadcrum a {
+        color: black;
+    }
 </style>
