2f05d3f7fb989d2b7564d4357ddeff25f2ef83e7
shinyeji28 <xntm10@naver.com>
2024-01-24 23:13:28 +0900

    refactor: controller로 로그인 코드 옮김

--- backend/Exhale/src/main/java/com/ssafy/exhale/security/LoginFilter.java
+++ backend/Exhale/src/main/java/com/ssafy/exhale/security/LoginFilter.java
@@ -1,11 +1,9 @@
 package com.ssafy.exhale.security;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ssafy.exhale.dto.logicDto.CustomUserDetails;
-import com.ssafy.exhale.dto.responseDto.MemberResponse;
-import com.ssafy.exhale.dto.responseDto.TokenInfo;
 import com.ssafy.exhale.util.JWTUtil;
 import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -14,25 +12,16 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import java.io.IOException;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-
 @RequiredArgsConstructor
 
 public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     private String usernameParameter = "login_id";
 
     private final AuthenticationManager authenticationManager;
-
-    private final JWTUtil jwtUtil;
-
-    private final ObjectMapper objectMapper;
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
@@ -52,49 +41,16 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     }
 
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
-        CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
-        String loginId = customUserDetails.getUsername();
-        int memberId = customUserDetails.getMemberId();
-        String nickname = customUserDetails.getNickname();
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain, Authentication authentication) throws ServletException, IOException {
 
-        //roll 값
-        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
-        Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
-        GrantedAuthority auth = iterator.next();
-        String role = auth.getAuthority();
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        String jwt = jwtUtil.createJwt(loginId, memberId, role);
-
-
-        TokenInfo tokeninfo = new TokenInfo("Bearer "+jwt,null);
-        MemberResponse memberResponse = new MemberResponse(memberId, loginId, nickname);
-
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-
-        Map<String, Object> responseBody = new HashMap<>();
-
-        responseBody.put("token",tokeninfo);
-        responseBody.put("member",memberResponse);
-
-
-        try{
-            String jsonResponse = objectMapper.writeValueAsString(responseBody);
-            response.getWriter().write(jsonResponse);
-
-        }catch (IOException e){
-            logger.error("Error while constructing and writing JSON response", e);
-            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-        }
-
-//        response.addHeader("Authorization", "Bearer "+jwt);
+        filterChain.doFilter(request, response);
 
     }
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
-
         response.setStatus(401);
     }
 }
