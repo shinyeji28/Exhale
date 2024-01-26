3e3e97e9d55d0bf5adbeee15b8a14fb5c6ea9ed7
조상민 <nice6122@naver.com>
2024-01-26 11:07:23 +0900

    feat: 페이지네이션 구현

--- frontend/Exhale/src/api/posts.js
+++ frontend/Exhale/src/api/posts.js
@@ -5,6 +5,13 @@ const posts = [
   {articleId: 3, title: '제목3', content: '내용3', create_date: '2020-03-01'},
   {articleId: 4, title: '제목4', content: '내용4', create_date: '2020-04-01'},
   {articleId: 5, title: '제목5', content: '내용5', create_date: '2020-05-01'},
+  {articleId: 6, title: '제목6', content: '내용6', create_date: '2020-01-01'},
+  {articleId: 7, title: '제목7', content: '내용7', create_date: '2020-02-01'},
+  {articleId: 8, title: '제목8', content: '내용8', create_date: '2020-03-01'},
+  {articleId: 9, title: '제목9', content: '내용9', create_date: '2020-04-01'},
+  {articleId: 10, title: '제목10', content: '내용10', create_date: '2020-05-01'},
+  {articleId: 11, title: '제목11', content: '내용11', create_date: '2020-02-08'},
+  {articleId: 12, title: '제목12', content: '내용12', create_date: '2020-03-02'}
 ]
 
 export function getPosts() {
---
+++ frontend/Exhale/src/components/functions/Pagination.vue
@@ -0,0 +1,105 @@
+<template>
+	<div style="display: flex; align-items: center;">
+		<span>
+			<a
+				@click="
+					() => {
+                        if (leftmostPage > 1) {
+						leftmostPage -= PAGE_PER_SECTION;
+						// onChangeCurPage(leftmostPage);
+                        					}}
+				"
+				>&lt;</a
+			>
+		</span>
+		<span class="page" v-for="page in getPaginationArray(leftmostPage)" :key="page">
+			<a
+				:class="{ on: page === curPage }"
+				@click="
+					() => {
+						onChangeCurPage(page);
+					}
+				"
+			>
+				{{ page }}</a
+			>
+		</span>
+		<span v-if="leftmostPage + PAGE_PER_SECTION <= totalPage">
+			<a
+				@click="
+					() => {
+                        // if (leftmostPage + PAGE_PER_SECTION <= totalPage){
+						leftmostPage += PAGE_PER_SECTION;
+						onChangeCurPage(leftmostPage);
+					}
+				"
+				>&gt;</a
+			>
+		</span>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	props: {
+		list: Array,
+		ITEM_PER_PAGE: Number,
+		PAGE_PER_SECTION: Number,
+	},
+	emits: ['change-page'],
+	setup(props, { emit }) {
+		return {
+			...usePagination(props, emit),
+		};
+	},
+};
+
+const usePagination = (props, emit) => {
+	const curPage = ref(1);
+	const leftmostPage = ref(1);
+	const totalPage = Math.ceil(props.list.length / (props.ITEM_PER_PAGE || 1));
+
+	const getPaginationArray = (left) => {
+		// [left,left+1,left+2...] 로 원소의 개수를 PAGE_PER_SECTION 개 만드는 함수
+		const res = [];
+		for (let i = left; i < Math.min(totalPage + 1, left + props.PAGE_PER_SECTION); i++) {
+			res.push(i);
+		}
+		return res;
+	};
+
+	const onChangeCurPage = (page) => {
+		curPage.value = page;
+		emit('change-page', page);
+	};
+
+	return {
+		leftmostPage,
+		curPage,
+		totalPage,
+		//
+		getPaginationArray,
+		onChangeCurPage,
+	};
+};
+</script>
+
+<style>
+.page {
+	padding: 1rem;
+	cursor: pointer;
+   
+    
+}
+.page a.on {
+	font-weight: 900;
+	color: red;
+}
+.page a, span > a {
+    cursor: pointer;
+    
+}
+
+</style>
