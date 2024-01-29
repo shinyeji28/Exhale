2f6a815bf182224837e8cdf88f8a012a04cfd285
HuiJung <tom1997tom@gmail.com>
2024-01-29 13:13:30 +0900

    feat: Article Custom Repository 구현
    ArticleReposittoryCustom, ArticleRepositoryCustomImpl 구현 완료.
    
    1. ArticleRepositoryCustom
    - QueryDSL을 사용한 query를 ArticleRepository에 상속하기 위해 ArticleRepositoryCustom 생성.
    2. ArticleRepositoryCustomImpl
    - ArticleRepositoryCustom에 정의된 쿼리를 구현하는 객체

---
+++ backend/Exhale/src/main/java/com/ssafy/exhale/repository/ArticleRepositoryCustomImpl.java
@@ -0,0 +1,45 @@
+package com.ssafy.exhale.repository;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.exhale.domain.Article;
+import com.ssafy.exhale.domain.QArticle;
+import com.ssafy.exhale.dto.requestDto.ArticleSearchRequest;
+import com.ssafy.exhale.dto.responseDto.ArticleListResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class ArticleRepositoryCustomImpl implements ArticleRepositoryCustom{
+    private final JPAQueryFactory queryFactory;
+    private final QArticle article = QArticle.article;
+    @Override
+    public List<Article> search(ArticleSearchRequest articleSearchRequest) {
+        PageRequest pageRequest = PageRequest.of(articleSearchRequest.getPage(), 10);
+
+        JPAQuery<Article> query =  queryFactory.select(article)
+                .from(article)
+                .where(article.board.id.eq(articleSearchRequest.getBoardId()))
+                .where(checkSearchType(articleSearchRequest.getSearchType(), articleSearchRequest.getSearchContent()))
+                .offset(pageRequest.getPageNumber() * 10L)
+                .limit(pageRequest.getPageSize());
+        return query.fetch();
+    }
+
+    private BooleanExpression checkSearchType(String searchType, String searchContent){
+        if(searchType.equals("title")){
+            return article.title.like("%" + searchContent + "%");
+        }
+        if(searchType.equals("content")){
+            return article.content.like("%" + searchContent + "%");
+        }
+        if(searchType.equals("author")){
+            return article.nickname.like("%" + searchContent + "%");
+        }
+        return null;
+    }
+
+}
