e36c69d4f7fe8f05b0d1af13abb995c3a82766c3
HuiJung <tom1997tom@gmail.com>
2024-01-30 15:46:20 +0900

    feat: 게시글 List 반환을 위한 ArticleListResponse Dto 로직 추가
    게시글 List 반환을 위한 ArticleListResponse Dto 로직 추가
    - of, from 메서드를 통한 dto 변환 로직 추가

--- backend/Exhale/src/main/java/com/ssafy/exhale/dto/responseDto/ArticleListResponse.java
+++ backend/Exhale/src/main/java/com/ssafy/exhale/dto/responseDto/ArticleListResponse.java
@@ -1,7 +1,9 @@
 package com.ssafy.exhale.dto.responseDto;
 
+import com.ssafy.exhale.dto.logicDto.ArticleDto;
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @Getter
@@ -10,11 +12,37 @@ import java.util.Date;
 @AllArgsConstructor
 @ToString
 public class ArticleListResponse {
-    int Id;
+    Integer id;
     String title;
     int view;
-    boolean isHidden;
-    int memberId;
     String nickname;
-    Date createDate;
+    LocalDateTime createDate;
+    String thumbnail;
+//    Integer memberId;
+
+    public static ArticleListResponse of(Integer id, String title, int view, /*Integer memberId,*/ String nickname,
+                                         LocalDateTime createDate, String thumbnail)
+    {
+        return new ArticleListResponse(
+                id,
+                title,
+                view,
+                nickname,
+                createDate,
+                thumbnail
+//                memberId,
+        );
+    }
+
+    public static ArticleListResponse from(ArticleDto articleDto){
+        return new ArticleListResponse(
+                articleDto.getId(),
+                articleDto.getTitle(),
+                articleDto.getView(),
+                articleDto.getNickname(),
+                articleDto.getCreateDate(),
+                articleDto.getThumbnail()
+//                articleDto.getMemberDto
+        );
+    }
 }
