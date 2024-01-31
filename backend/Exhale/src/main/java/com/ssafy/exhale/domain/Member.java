3103f072e085bf34871119a444a813f3078c0291
HuiJung <tom1997tom@gmail.com>
2024-01-31 09:22:37 +0900

    style: Member Entity 불필요한 import 삭제
    Member Entity 불필요한 import 삭제

--- backend/Exhale/src/main/java/com/ssafy/exhale/domain/Member.java
+++ backend/Exhale/src/main/java/com/ssafy/exhale/domain/Member.java
@@ -1,44 +1,72 @@
 package com.ssafy.exhale.domain;
 
+import com.ssafy.exhale.dto.requestDto.MemberRequest;
 import jakarta.persistence.*;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
-@Setter
 @Table(name = "member")
+@NoArgsConstructor
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="member_id")
-    private int memberId;
-    @Column(name="login_id")
+    @Column(name = "member_id")
+    private Long id;
+    @Column(name = "login_id")
     private String loginId;
     private String password;
     private String name;
-    @Column(name="email_id")
+    @Column(name = "email_id")
     private String emailId;
-    @Column(name="email_domain")
+    @Column(name = "email_domain")
     private String emailDomain;
     private String birth;
     private String nickname;
-    @Column(name="profile_img", nullable = true)
+    @Column(name = "profile_img", nullable = true)
     private String profileImg;
-    @Column(name="is_active", nullable = true, columnDefinition = "tinyint default true")
-    private boolean isActive;
-    @Column(name="refresh_value", nullable = true)
+    @Column(name = "withdraw", nullable = true, columnDefinition = "TINYINT(1) default 0")
+    private Boolean withdraw;
+    @Column(name = "refresh_value", nullable = true)
     private String refreshValue;
     @CreatedDate
-    @Column(name="create_date") 
+    @Column(name = "create_date")
     private LocalDateTime createDate;
-    @Column(name="withdrawal_date", nullable = true)
-    private LocalDateTime withdrawal_date;
+    @Column(name = "withdraw_date", nullable = true)
+    private LocalDateTime withdrawDate;
     private String role;
 
+    public void updateWithdraw(boolean withdrawal) {
+        if (withdrawal) this.withdraw = true;
+        else this.withdraw = false;
+    }
 
+    public void updateJoin(MemberRequest memberRequest, String password) {
+        this.name = memberRequest.getName();
+        this.birth = memberRequest.getBirth();
+        this.emailDomain = memberRequest.getEmailDomain();
+        this.emailId = memberRequest.getEmailId();
+        this.loginId = memberRequest.getLoginId();
+        this.password = password;
+        this.role = "ROLE_USER";
+    }
 
+    public void updateRefreshValue(String value) {
+        this.refreshValue = value;
+    }
+
+    public void capsulePayload(String loginId, long memberId, String role) {
+        this.loginId = loginId;
+        this.id = memberId;
+        this.role = role;
+    }
+
+    public void updatePassword(String password) {
+        this.password = password;
+    }
 }
+
