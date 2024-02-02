33f14d68e539bde60e651fadb5e836a6a5c5e073
shinyeji28 <xntm10@naver.com>
2024-02-02 15:06:31 +0900

    fix: origin 수정

--- backend/Exhale/src/main/java/com/ssafy/exhale/config/SecurityConfig.java
+++ backend/Exhale/src/main/java/com/ssafy/exhale/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration configuration = new CorsConfiguration();
-                        configuration.setAllowedOrigins(Collections.singletonList("*"));
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
                         configuration.setAllowedMethods(Collections.singletonList("*"));
                         configuration.setAllowCredentials(true);
                         configuration.setAllowedHeaders(Collections.singletonList("*"));
