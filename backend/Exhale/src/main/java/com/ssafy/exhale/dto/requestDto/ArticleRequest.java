f8251da2447acf27a17829a66d5f8f9dfe00a646
HuiJung <tom1997tom@gmail.com>
2024-02-02 10:10:29 +0900

    refactor: Request 객체 필드 변수들 snake_case json 타입과 매핑
    Request 객체 필드 변수들 snake_case json 타입과 매핑

--- backend/Exhale/src/main/java/com/ssafy/exhale/dto/requestDto/ArticleRequest.java
+++ backend/Exhale/src/main/java/com/ssafy/exhale/dto/requestDto/ArticleRequest.java
@@ -1,5 +1,6 @@
 package com.ssafy.exhale.dto.requestDto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.ssafy.exhale.dto.logicDto.ArticleDto;
 import com.ssafy.exhale.dto.logicDto.BoardDto;
 import com.ssafy.exhale.dto.logicDto.MemberDto;
@@ -14,9 +15,11 @@ public class ArticleRequest {
     private String title;
     private String content;
     private String thumbnail;
+    @JsonProperty("board_id")
     private Integer boardId;
 
     //작성자 정보 부분
+    @JsonProperty("member_id")
     private Long memberId;
     private String nickname;
 
--- backend/Exhale/src/main/java/com/ssafy/exhale/dto/requestDto/ArticleSearchRequest.java
+++ backend/Exhale/src/main/java/com/ssafy/exhale/dto/requestDto/ArticleSearchRequest.java
@@ -1,5 +1,6 @@
 package com.ssafy.exhale.dto.requestDto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Getter
@@ -8,8 +9,11 @@ import lombok.*;
 @AllArgsConstructor
 @ToString
 public class ArticleSearchRequest {
+    @JsonProperty("board_id")
     int boardId;
+    @JsonProperty("search_type")
     String searchType;
+    @JsonProperty("search_content")
     String searchContent;
     int page;
 }
