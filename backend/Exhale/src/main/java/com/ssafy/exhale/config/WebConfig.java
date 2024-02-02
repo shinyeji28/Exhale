c2e012a304114215dba50be2b529fd27aa39bf01
shinyeji28 <xntm10@naver.com>
2024-02-02 14:46:25 +0900

    feat: cors 설정

---
+++ backend/Exhale/src/main/java/com/ssafy/exhale/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.ssafy.exhale.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")  
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
