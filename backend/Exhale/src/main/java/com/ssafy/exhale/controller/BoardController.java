b1e0b5807620abbde2733d6eb2792675c0f3fe78
HuiJung <tom1997tom@gmail.com>
2024-02-05 15:29:52 +0900

    refactor: Board, Article Controller 분리
    Board, Article Controller 분리

--- backend/Exhale/src/main/java/com/ssafy/exhale/controller/BoardController.java
+++ backend/Exhale/src/main/java/com/ssafy/exhale/controller/BoardController.java
@@ -1,16 +1,12 @@
 package com.ssafy.exhale.controller;
 
-import com.ssafy.exhale.dto.requestDto.ArticleRequest;
 import com.ssafy.exhale.dto.requestDto.ArticleSearchRequest;
 import com.ssafy.exhale.dto.responseDto.commonDto.CommonResponse;
 import com.ssafy.exhale.service.ArticleService;
 import com.ssafy.exhale.service.BoardService;
-import com.ssafy.exhale.util.TokenPayloadUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RestController
@@ -19,7 +15,6 @@ public class BoardController {
 
     private final BoardService boardService;
     private final ArticleService articleService;
-    private final TokenPayloadUtil tokenPayloadUtil;
 
     //게시판 종류
     @GetMapping
@@ -37,47 +32,5 @@ public class BoardController {
     @PostMapping("/search")
     public ResponseEntity<CommonResponse> search(@RequestBody ArticleSearchRequest articleSearchRequest){
         return CommonResponse.ok(articleService.search(articleSearchRequest));
-    }
-
-    //게시글 조회
-    @Transactional(readOnly = true)
-    @GetMapping("/articles/{article_id}")
-    public ResponseEntity<CommonResponse> getArticle(@PathVariable("article_id") Long articleId){
-        return CommonResponse.ok(articleService.getArticle(articleId));
-    }
-
-    //게시글 생성
-    @PostMapping("/articles")
-    public ResponseEntity<CommonResponse> postArticle(@RequestBody ArticleRequest articleRequest){
-
-        articleService.postArticle(articleRequest, tokenPayloadUtil.getMemberId());
-        return CommonResponse.ok(null);
-    }
-
-    //게시글 수정
-    @PutMapping("/articles/{article_id}")
-    public ResponseEntity<CommonResponse> modifyArticle(@PathVariable("article_id") Long articleId,@RequestBody ArticleRequest articleRequest){
-        articleService.modifyArticle(articleId, articleRequest, tokenPayloadUtil.getMemberId());
-        return CommonResponse.ok(null);
-    }
-
-    //게시글 삭제처리
-    @DeleteMapping("/articles/{article_id}")
-    public ResponseEntity<CommonResponse> deleteArticle(@PathVariable("article_id") Long articleId){
-        articleService.deleteArticle(articleId, tokenPayloadUtil.getMemberId());
-        return CommonResponse.ok(null);
-    }
-
-    @PostMapping("/articles/image/{article_id}")
-    public ResponseEntity<CommonResponse> saveImage(@PathVariable("article_id") Long articleId, @RequestParam(value = "file") MultipartFile file){
-        System.out.println("file = " + file.getOriginalFilename());
-        String imageURL = articleService.saveImage(file, articleId);
-        return CommonResponse.ok(imageURL);
-    }
-
-    @DeleteMapping("/articles/image/{article_file_id}")
-    public ResponseEntity<CommonResponse> deleteImage(@PathVariable("article_file_id") Long articleFileId){
-        articleService.deleteImage(articleFileId);
-        return CommonResponse.ok(null);
     }
 }
