28376716b01b5079e9aaef75ecd218751f2d714f
shinyeji28 <xntm10@naver.com>
2024-02-08 14:27:31 +0900

    feat: 타이머, 데이터 셋 구성

--- frontend/Exhale/src/views/ARC/SayObject.vue
+++ frontend/Exhale/src/views/ARC/SayObject.vue
@@ -1,57 +1,123 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, onBeforeUnmount  } from 'vue';
 import { storeToRefs } from "pinia";
 import { useAuthStore } from "@/stores/auth";
 import { getCourseList, getProblem  } from '@/api/course.js';
 import STT from '@/components/ARC/STT.vue';
 import TTS from '@/components/ARC/TTS.vue';
 
+// todo routing으로 받기
+const courseName='';
+const categoryName='';
+const overTime = 10;
+const categoryId = 1; 
 
-// const authStore = useAuthStore();
-// const { JWTtoken } = storeToRefs(authStore);
-// // const token = JWTtoken.value;
-// const token ="Bearer eyJhbGciOiJIUzI1NiJ9.eyJsb2dpbl9pZCI6InNzYWZ5MTIiLCJtZW1iZXJfaWQiOjMsInJvbGUiOiJST0xFX1VTRVIiLCJpYXQiOjE3MDcyNjQ2NzIsImV4cCI6MTcwNzI2NjQ3Mn0.E0kf1R_x_jHCJ9KJ8MP4hQU084U8KAtGb2Pefs_bLCc"; 
+const authStore = useAuthStore();
+const { JWTtoken } = storeToRefs(authStore);
+const token = JWTtoken.value;
 
-// const saveProblem = async () => {
-//   try {
-//     const { data } = await getProblem(1, token);
-//     console.log(data);
-//   } catch (error) {
-//     console.error(error); 
-//   }
-// };
-// saveProblem();
+let problemIdx=0;
+let problemSet=null;
+let no = ref(1);
+const problem = {
+  answer: ref(''),
+  hint: ref(''),
+  img_url : ref('')
+};
 
-// const getCourses = async () => {
-//   try {
-//     const { data } = await getCourseList(token);
-//     console.log(data);
-//   } catch (error) {
-//     console.error(error); 
-//   }
-// };
-// getCourses();
+const elapsedTime = ref(overTime);
+let timerId;
+
+// 컴포넌트가 마운트될 때 시작하는 타이머 설정
+const startTimer = () => {
+  timerId = setInterval(() => {
+    elapsedTime.value--;
+    if (elapsedTime.value < 0) {
+      clearInterval(timerId);
+      alert('타이머 종료');
+    }
+  }, 1000);
+};
+
+// 컴포넌트가 언마운트될 때 타이머 정리
+const stopTimer = () => {
+  clearInterval(timerId);
+};
+
+
+const saveProblem = async () => {
+  try {
+    const { data } = await getProblem(categoryId, token);
+    if(data.dataStatus.code!=1){
+      // todo api 응답 예외 처리
+      return;
+    }
+    problemIdx = data.response.first_problem_index;
+    problemSet = data.response.problemResponseList;
+
+    problem.answer.value = problemSet[problemIdx].answer;
+    problem.hint.value = problemSet[problemIdx].hint;
+    problem.img_url.value = problemSet[problemIdx].img_url;
+
+  } catch (error) {
+    console.error(error); 
+  }
+};
 
 // STT
 const contentField = ref("")
 
 const handleContentFieldChange = (value) => {
   contentField.value = value;
+  // todo 음성 녹음 종료 시 정답 오답 처리
+  nextProblem();
 };
 
+onMounted(startTimer);
+onBeforeUnmount(stopTimer);
+
+
+saveProblem();
+
+
+
+
+
 // TTS
-const ttsText = ref("안녕하세요");
+// const ttsText = ref("안녕하세요");
+
+const nextProblem = () => {
+  if(problemIdx>=problemSet.length){
+    // todo 게임 종료
+    console.log('게임 종료')
+    return;
+  }
+  console.log(problemIdx)
+
+  problem.answer.value = problemSet[problemIdx].answer;
+  problem.hint.value = problemSet[problemIdx].hint;
+  problem.img_url.value = problemSet[problemIdx].img_url;
+  problemIdx++;
+  no.value++;
+
+}
 
 </script>
 
 <template>
-    <div>
+    <div v-if="problem">
+      <h1>경과 시간: {{ elapsedTime }}</h1>
+      <div>{{ no }}. 아래 이미지가 나타내는 적합한 단어를 말하세요. </div>
       <STT 
         @update:modelValue="handleContentFieldChange"
         />
-      <TTS
+      <div>{{ problem.answer.value }}</div>
+      <div>{{ problem.hint.value }}</div>
+      <div><img :src="problem.img_url.value"/></div>
+      <!-- <button @click="nextProblem">다음</button> -->
+      <!-- <TTS
         :tts-text="ttsText"
-      />
+      /> -->
     </div>
 </template>
 
--- frontend/Exhale/src/views/main/ARCWaitingPage.vue
+++ frontend/Exhale/src/views/main/ARCWaitingPage.vue
@@ -5,7 +5,15 @@
 </template>
 
 <script setup>
-
+// const getCourses = async () => {
+//   try {
+//     const { data } = await getCourseList(token);
+//     console.log(data);
+//   } catch (error) {
+//     console.error(error); 
+//   }
+// };
+// getCourses();
 </script>
 
 <style lang="scss" scoped>
