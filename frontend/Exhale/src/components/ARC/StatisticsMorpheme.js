dc1e1e169ac9aa4c4dc4a52d194ec58e34fb21f4
shinyeji28 <xntm10@naver.com>
2024-02-12 23:29:23 +0900

    fix: 통계 데이터 수정

--- frontend/Exhale/src/components/ARC/StatisticsMorpheme.js
+++ frontend/Exhale/src/components/ARC/StatisticsMorpheme.js
@@ -4,35 +4,23 @@ import { compareMorpheme } from '@/components/ARC/Morpheme.js';
 
 function generateStatistics(morphemeList, answer, text) {
     const differences = compareMorpheme(answer, text);
-    const statistics = {};
-    console.log(morphemeList)
-    // 모든 형태소에 대한 초기 통계 정보를 생성
-    // 
-    
+    const list = [];
 
     // 형태소 차이를 기반으로 통계 정보 업데이트
     differences.forEach(({ type, morpheme1, morpheme2 }) => {
         // morpheme1과 morpheme2를 기반으로 해당하는 letter_id 찾기
-        const letter_id1 = morphemeList.find(morpheme => morpheme.letter_char === morpheme1 && morpheme.type === type)?.letter_id;
-        const letter_id2 = morphemeList.find(morpheme => morpheme.letter_char === morpheme2 && morpheme.type === type)?.letter_id;
+        const letter_id = morphemeList.find(morpheme => morpheme.letter_char === morpheme1 && morpheme.type === type)?.letter_id;
+        const statistics = {};
 
-        // 찾은 letter_id가 존재할 경우 해당하는 통계 정보 업데이트
-        console.log(letter_id1, letter_id2)
-        if (letter_id1) {
-            if (type === 'cho' || type === 'jung' || type === 'jong') {
-                statistics[letter_id1].wrong_cnt++;
-            } else {
-                statistics[letter_id1].correct_cnt++;
-            }
+        if (!statistics[letter_id]) {
+            statistics[letter_id] = { letter_id: letter_id, correct_cnt: 0, wrong_cnt: 0 };
         }
-
-        if (letter_id2) {
-            if (type === 'cho' || type === 'jung' || type === 'jong') {
-                statistics[letter_id2].wrong_cnt++;
-            } else {
-                statistics[letter_id2].correct_cnt++;
-            }
+        if (morpheme1 === morpheme2) {
+            statistics[letter_id].correct_cnt++;
+        } else {
+            statistics[letter_id].wrong_cnt++;
         }
+        list.push(statistics[letter_id]);
     });
 
     return Object.values(statistics);
