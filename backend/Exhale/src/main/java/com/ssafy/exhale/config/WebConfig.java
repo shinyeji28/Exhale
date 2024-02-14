31957065b7f949889f9c102bb7feafaf9356b8f2
정건준 <dlans321@naver.com>
2024-02-14 20:52:17 +0900

    fix: cors

--- backend/Exhale/src/main/java/com/ssafy/exhale/config/WebConfig.java
+++ backend/Exhale/src/main/java/com/ssafy/exhale/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")  
-                .allowedOrigins("http://192.168.192.4")
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
